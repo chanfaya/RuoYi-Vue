2c8fc79e2595d198a0a39a5e6361a68f8619a0c0
chanfaya <chanfaya@outlook.com>
2024-06-17 00:58:51 +0800

    修改redis 配置 删除集群

--- ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/config/properties/RedissonProperties.java
+++ ruoyi-common/ruoyi-common-redis/src/main/java/org/dromara/common/redis/config/properties/RedissonProperties.java
@@ -35,10 +35,6 @@ public class RedissonProperties {
      */
     private SingleServerConfig singleServerConfig;
 
-    /**
-     * 集群服务配置
-     */
-    private ClusterServersConfig clusterServersConfig;
 
     @Data
     @NoArgsConstructor
@@ -73,62 +69,6 @@ public class RedissonProperties {
          * 发布和订阅连接池大小
          */
         private int subscriptionConnectionPoolSize;
-
-    }
-
-    @Data
-    @NoArgsConstructor
-    public static class ClusterServersConfig {
-
-        /**
-         * 客户端名称
-         */
-        private String clientName;
-
-        /**
-         * master最小空闲连接数
-         */
-        private int masterConnectionMinimumIdleSize;
-
-        /**
-         * master连接池大小
-         */
-        private int masterConnectionPoolSize;
-
-        /**
-         * slave最小空闲连接数
-         */
-        private int slaveConnectionMinimumIdleSize;
-
-        /**
-         * slave连接池大小
-         */
-        private int slaveConnectionPoolSize;
-
-        /**
-         * 连接空闲超时，单位：毫秒
-         */
-        private int idleConnectionTimeout;
-
-        /**
-         * 命令等待超时，单位：毫秒
-         */
-        private int timeout;
-
-        /**
-         * 发布和订阅连接池大小
-         */
-        private int subscriptionConnectionPoolSize;
-
-        /**
-         * 读取模式
-         */
-        private ReadMode readMode;
-
-        /**
-         * 订阅模式
-         */
-        private SubscriptionMode subscriptionMode;
 
     }
 
